fa351e5279bffbf01c5b55557e88349e7f68b6d5
jp-96 <jp-96@outlook.jp>
2022-07-03 15:35:27 +0900

    GateJudge

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Logo from './Logo';
 //import './App.css'; // ==> ../index.html
+import GateJudge from './components/GateJudge';
 
 function App() {
   return (
@@ -18,6 +19,11 @@ function App() {
         >
           Learn React
         </a>
+        <GateJudge gate='11' />
+        <GateJudge gate='12' defaultPenalty='0' />
+        <GateJudge gate='13' defaultPenalty='2' />
+        <GateJudge gate='14' defaultPenalty='50' gateColor='red' />
+        <GateJudge gate='15' defaultPenalty='DNF' />
         <Logo />
       </header>
     </div>
---
+++ src/app/components/GateJudge.tsx
@@ -0,0 +1,16 @@
+import React, {useState} from "react";
+
+export default function GateJudge({ gate, gateColor='green', defaultPenalty = ''}) {
+    const [penalty, setPenalty] = useState(defaultPenalty);
+    const onGateJudge = penalty => setPenalty(penalty);
+    return (
+        <div className='gate-penalty'>
+            <span className={'gate-penalty-gate ' +(gateColor=='green' ? 'gate-color-green' : 'gate-color-red')}>{gate}</span>
+            <span className={'gate-penalty-0 ' + (penalty == '0' ? 'gate-penalty-selected' : 'gate-penalty-unselected')} onClick={() => onGateJudge('0')} >0</span>
+            <span className={'gate-penalty-2 '  + (penalty == '2' ? 'gate-penalty-selected' : 'gate-penalty-unselected')} onClick={() => onGateJudge('2')} >2</span>
+            <span className={'gate-penalty-50 '  + (penalty == '50' ? 'gate-penalty-selected' : 'gate-penalty-unselected')} onClick={() => onGateJudge('50')} >50</span>
+            <span className={'gate-penalty-dnf ' + (penalty == 'DNF' ? 'gate-penalty-selected' : 'gate-penalty-unselected')} onClick={() => onGateJudge('DNF')} >DNF</span>
+        </div>
+    );
+
+}
