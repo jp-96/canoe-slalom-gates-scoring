ae1b2af426a998ede16816f352da54867dea21bd
jp-96 <jp-96@outlook.jp>
2022-07-06 13:56:28 +0000

    color

--- src/app/hooks.tsx
+++ src/app/hooks.tsx
@@ -3,19 +3,6 @@ import Sheetdata from '../api/penaltydata';
 import { GASClient } from 'gas-client';
 const { serverFunctions } = new GASClient();
 
-interface inputProps {
-  value: any;
-  onChange: (e: any) => void;
-};
-
-export const useInput = (initialValue): [inputProps, () => void] => {
-  const [value, setValue] = useState(initialValue);
-  return [
-    { value, onChange: e => setValue(e.target.value) },
-    () => setValue(initialValue)
-  ];
-};
-
 export const useGetSheetData = (sheetName: string, beginGate: number, gateLength: number): { loading: boolean, error: any, sections: Sheetdata.section[], setSections: React.Dispatch<React.SetStateAction<Sheetdata.section[]>> } => {
   const emptySections: Sheetdata.section[] = [];
   const [loading, setLoading] = useState(true);
