8a4a6f40e4560e494d583c88cd987124c87bd2bb
jp-96 <jp-96@outlook.jp>
2022-08-14 00:30:45 +0000

    del

--- src/app/components/MuiTimeInput.tsx
+++ src/app/components/MuiTimeInput.tsx
@@ -4,7 +4,6 @@ import Typography from "@mui/material/Typography";
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import { styled, useTheme } from '@mui/material/styles';
-import NumberFormat from "react-number-format";
 import { grey, green, red, blue, } from '@mui/material/colors';
 import { useData } from "../providers/CanoeSlalomHeatDataProvider";
 import MuiTimeInputDialog, { TimeJudgeData } from './MuiTimeInputDialog';
@@ -30,88 +29,6 @@ function TimeLabel({ startOrFinish, isLocked = false }) {
     );
 }
 
-function TimeInput({ seconds, isError = false, isFailure = false, isLoading = false, isLocked = false, onChanged = f => f }) {
-    const color = isLoading ? 'secondary' : isFailure ? 'warning' : isError ? 'error' : 'primary';
-
-    const hms = CanoeSlalomHeatData.secondsToHms(seconds);
-    const hmsValue = (hms.hours * 100 + hms.minutes) * 100 + hms.seconds;
-
-    return (
-        <Stack direction="row" spacing={'-1px'} sx={{ m: '0px', }}>
-            <NumberFormat
-                className="hours"
-                value={hms.hours}
-            />
-            <NumberFormat
-                className="minutes"
-                value={hms.minutes}
-            />
-            <NumberFormat
-                className="seconds"
-                value={hms.seconds}
-                decimalScale={3}
-                fixedDecimalScale={true}
-            />
-        </Stack>
-    );
-}
-
-function JudgeButton({ startOrFinish, judge, isError = false, isFailure = false, isLoading = false, isLocked = false, onSelected = f => f }) {
-    const onSelectedStarted = () => onSelected('STARTED');
-    const onSelectedDns = () => onSelected('DNS');
-    const onSelectedFinished = () => onSelected('FINISHED');
-    const onSelectedFinished50 = () => onSelected('FINISHED_50');
-    const onSelectedDnf = () => onSelected('DNF');
-    const onSelectedDsq = () => onSelected('DSQ');
-
-    const color = isLoading ? 'secondary' : isFailure ? 'warning' : isError ? 'error' : 'primary'
-    const sx = {
-        width: '64px',
-        height: '42px',
-        fontSize: '1.2rem',
-    };
-    const sxLeft = {
-        ...sx,
-        borderTopRightRadius: 0,
-        borderBottomRightRadius: 0,
-    };
-    const sxMiddle = {
-        ...sx,
-        borderRadius: 0,
-    };
-    const sxRight = {
-        ...sx,
-        borderTopLeftRadius: 0,
-        borderBottomLeftRadius: 0,
-    };
-
-    const StartedButton = () => (
-        <Button variant={judge === 'STARTED' ? 'contained' : 'outlined'} disabled={isLocked} color={color} sx={{ ...sxLeft, width: '127px', }} onClick={onSelectedStarted}>ENT</Button>
-    );
-    const FinishedButton = () => (
-        <Button variant={judge === 'FINISHED' ? 'contained' : 'outlined'} disabled={isLocked} color={color} sx={sxLeft} onClick={onSelectedFinished}>ENT</Button>
-    );
-    const Finished50Button = () => (
-        <Button variant={judge === 'FINISHED_50' ? 'contained' : 'outlined'} disabled={isLocked} color={color} sx={sxMiddle} onClick={onSelectedFinished50}>+50</Button>
-    );
-    const DnsButton = () => (
-        <Button variant={judge === 'DNS' ? 'contained' : 'outlined'} disabled={isLocked} color={color} sx={sxMiddle} onClick={onSelectedDns}>DNS</Button>
-    );
-    const DnfButton = () => (
-        <Button variant={judge === 'DNF' ? 'contained' : 'outlined'} disabled={isLocked} color={color} sx={sxMiddle} onClick={onSelectedDnf}>DNF</Button>
-    );
-    const DsqButton = () => (
-        <Button variant={judge === 'DSQ' ? 'contained' : 'outlined'} disabled={isLocked} color={color} sx={sxRight} onClick={onSelectedDsq}>DSQ</Button>
-    );
-
-    return (
-        <Stack spacing={'-1px'} direction="row" sx={{ m: '0px' }}>
-            {startOrFinish === 'START' ? (<StartedButton />) : (<><FinishedButton /><Finished50Button /></>)}
-            {startOrFinish === 'START' ? (<><DnsButton /><DsqButton /></>) : (<><DnfButton /><DsqButton /></>)}
-        </Stack>
-    );
-}
-
 function TimeField({ bib, tag, startOrFinish, seconds, judge, isError = false, isFailure = false, isLoading = false, isLocked = false, onChangeTimeJudge = (s, j) => { } }) {
 
     const hms = CanoeSlalomHeatData.secondsToHms(seconds);
@@ -130,18 +47,24 @@ function TimeField({ bib, tag, startOrFinish, seconds, judge, isError = false, i
     const [open, setOpen] = React.useState(false);
 
     const handleEdit = () => {
+        if (isLocked) {
+            return;
+        }
         let hh: number | undefined = undefined;
         let mm: number | undefined = undefined;
         let ss: number | undefined = undefined;
         const judge = timeJudgeData.judge;
-        if (hms.hours > 0) {
-            hh = hms.hours;
+        const h = timeJudgeData.hh ? Number(timeJudgeData.hh) : 0;
+        const m = timeJudgeData.mm ? Number(timeJudgeData.mm) : 0;
+        const s = timeJudgeData.ss ? Number(timeJudgeData.ss) : 0;
+        if (h > 0) {
+            hh = h;
         }
-        if ((hms.hours > 0) || (hms.minutes > 0)) {
-            mm = timeJudgeData.mm;
+        if ((h > 0) || (m > 0)) {
+            mm = m;
         }
-        if ((hms.hours > 0) || (hms.minutes > 0) || (hms.seconds > 0)) {
-            ss = timeJudgeData.ss;
+        if ((h > 0) || (m > 0) || (s > 0)) {
+            ss = s;
         }
         setTimeJudgeData({ hh, mm, ss, judge, });
         setOpen(true);
@@ -150,7 +73,9 @@ function TimeField({ bib, tag, startOrFinish, seconds, judge, isError = false, i
     const onClose = (apply: boolean) => {
         setOpen(false);
         if (apply) {
-            const seconds: number = (Number(timeJudgeData.hh) * 3600) + (Number(timeJudgeData.mm) * 60) + Number(timeJudgeData.ss);
+            const seconds = (timeJudgeData.hh ? Number(timeJudgeData.hh) * 3600 : 0)
+                + (timeJudgeData.mm ? Number(timeJudgeData.mm) * 60 : 0)
+                + (timeJudgeData.ss ? Number(timeJudgeData.ss) : 0);
             onChangeTimeJudge(seconds, timeJudgeData.judge);
         }
     };
@@ -249,23 +174,11 @@ export default function MuiTimeInput({ row, bib, tag, startOrFinish, seconds, ju
             }
         }
     };
-    const onChangedTime = (seconds) => {
-        onChangeTimeJudge(seconds, judge);
-    }
-    const onSelectedJudge = (judge) => {
-        onChangeTimeJudge(seconds, judge);
-    }
 
     return (
-        <>
-            <Stack direction="row" spacing={1} sx={{ m: '0px' }}>
-                <TimeLabel startOrFinish={startOrFinish} isLocked={isLocked} />
-                <TimeField bib={bib} tag={tag} startOrFinish={startOrFinish} seconds={seconds} judge={judge} isError={isError} isFailure={isFailure} isLoading={isLoading} isLocked={isLocked} onChangeTimeJudge={onChangeTimeJudge} />
-            </Stack>
-            <Stack direction="column" spacing={1} sx={{ m: '0px' }}>
-                <TimeInput seconds={seconds} isError={isError} isFailure={isFailure} isLoading={isLoading} isLocked={isLocked} onChanged={onChangedTime} />
-                <JudgeButton startOrFinish={startOrFinish} judge={judge} isError={isError} isFailure={isFailure} isLoading={isLoading} isLocked={isLocked} onSelected={onSelectedJudge} />
-            </Stack>
-        </>
+        <Stack direction="row" spacing={1} sx={{ m: '0px' }}>
+            <TimeLabel startOrFinish={startOrFinish} isLocked={isLocked} />
+            <TimeField bib={bib} tag={tag} startOrFinish={startOrFinish} seconds={seconds} judge={judge} isError={isError} isFailure={isFailure} isLoading={isLoading} isLocked={isLocked} onChangeTimeJudge={onChangeTimeJudge} />
+        </Stack>
     );
 }
